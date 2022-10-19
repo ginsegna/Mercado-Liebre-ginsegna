967381e72b38a2145d6659783aa1a279adf43d5d
ginsegna <giuliink@gmail.com>
2022-10-19 18:25:31 -0300

    Primer commit - subiendo los archivos

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {console.log("Servidor en el puerto ${port}")});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
